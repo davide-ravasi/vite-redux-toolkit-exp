2cd623304d2211ede87014ba16b1a22841c0d42c
davide-ravasi <info@davideravasi.com>
2023-05-16 08:30:21 +0200

    feat: added fetchUSers

--- src/components/UsersList.tsx
+++ src/components/UsersList.tsx
@@ -1,4 +1,14 @@
+import { useDispatch } from "react-redux";
+import { useEffect } from "react";
+import { fetchUsers } from "../store";
+
 const UsersList = () => {
+    const dispatch = useDispatch();
+
+    useEffect(() => {
+        dispatch(fetchUsers());
+    }, [dispatch]);
+
     return (
         <div>UsersList</div>    
     )
--- src/store/thunks/fetchUsers.js
+++ src/store/thunks/fetchUsers.js
@@ -1,6 +1,18 @@
 import { createAsyncThunk } from "@reduxjs/toolkit";
 import axios from "axios";
 
+/*
+  to create a thunk
+  1 - create new file for your thunk. Name it after the purpose of your request
+  2 - import createAsyncThunk from @reduxjs/toolkit
+  3 - create a thunk using createAsyncThunk. Give it a base type that describe the purpose of the thunk
+  4 - make the thunk async and use axios to make the request
+  5 - export the thunk
+  6 - in the slice file, add the thunk to the extraReducers
+  7 - in it watch the action types that are dispatched by the thunk
+  8 - export the thunk from the store/index.js file
+  */
+
 
 const fetchUsers = createAsyncThunk("users/fetch", async () => {
   const response = await axios.get("http://localhost:3005/users");
@@ -22,6 +34,7 @@ const fetchUsers = createAsyncThunk("users/fetch", async () => {
   thunk is used to handle async operations
 
   createAsyncThunk automatically dispatches 3 actions
+  these are the action types that are dispatched
     1. users/fetch/pending
     2. users/fetch/fulfilled
     3. users/fetch/rejected

f8a32dd19b785b0099eb0aac1cf7106e67beaa64
davide-ravasi <info@davideravasi.com>
2023-05-23 08:27:19 +0200

    feat: added static button

--- src/App.tsx
+++ src/App.tsx
@@ -1,9 +1,16 @@
 import './App.css'
 import UsersList from './components/UsersList';
+import Button from './components/Button';
 
 function App() {
   return (
     <div className="container mx-auto">
+      <div className="text-2xl font-bold my-4 mx-2">
+        Users List
+        <Button primary className="ml-2 float-right">
+          Add User
+        </Button>
+      </div>
       <UsersList />
     </div>
   )
